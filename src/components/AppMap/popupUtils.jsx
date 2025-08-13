7bdadc6e3fa9f663c603f1d0cbfad1b0a0c8c574
“[Stanislav <52444330+nuclearkofola@users.noreply.github.com>
2025-08-13 22:56:59 +0200

    uprava popupu

--- src/components/AppMap/popupUtils.jsx
+++ src/components/AppMap/popupUtils.jsx
@@ -11,11 +11,12 @@ export const createPopupContent = (feature) => {
     : '';
 
   return `<div class="popup-content p-3 bg-white rounded">
-      <h2 class="text-lg font-bold">${name}</h2>
-      ${desc ? `<p class="mt-2 text-sm">${desc}</p>` : ''}
-      <p class="text-xs text-base-content mt-1">${address}</p>
-      ${url ? `<p class="mt-1 text-sm">${url}</p>` : ''}
-      ${propertiesList ? `<div class="properties text-xs mt-1">${propertiesList}</div>` : ''}
       ${image}
+      <h2 class="text-2xl font-bold my-3 text-primary">${name}</h2>
+      <div class="divider my-2"></div>
+      ${desc ? `<p class="my-3 text-base">${desc}</p><div class="divider my-2"></div>` : ''}
+      <p class="text-sm text-base-content my-2">${address}</p>
+      ${url ? `<p class="mt-3 text-sm">${url}</p>` : ''}
+      ${propertiesList ? `<div class="divider my-2"></div><div class="properties text-sm my-2">${propertiesList}</div>` : ''}
     </div>`;
 };

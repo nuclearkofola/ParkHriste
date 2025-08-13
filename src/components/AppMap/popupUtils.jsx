797880b045c5b0b20bd643b67643b710475e8bc8
“[Stanislav <52444330+nuclearkofola@users.noreply.github.com>
2025-08-13 23:30:32 +0200

    uprava kolize headru a popup v mobilni ver.

--- src/components/AppMap/popupUtils.jsx
+++ src/components/AppMap/popupUtils.jsx
@@ -4,15 +4,15 @@ export const createPopupContent = (feature) => {
   const address = props.address?.address_formatted || 'Adresa není k dispozici';
   const desc = props.content || props.perex || '';
   const url = props.url ? `<a href="${props.url}" target="_blank" class="link link-primary">Web</a>` : '';
-  const image = props.image?.url ? `<img src="${props.image.url}" alt="${name}" class="mt-2 w-full h-auto rounded" />` : '';
+  const image = props.image?.url ? `<img src="${props.image.url}" alt="${name}" class="mt-4 w-[90%] mx-auto h-auto rounded block" />` : '';
 
   const propertiesList = props.properties?.length 
     ? props.properties.map(p => `<p><b>${p.description}:</b> ${p.value}</p>`).join('') 
     : '';
 
-  return `<div class="popup-content p-3 bg-white rounded">
+  return `<div class="popup-content p-4 bg-white rounded">
       ${image}
-      <h2 class="text-2xl font-bold my-3 text-primary">${name}</h2>
+      <h2 class="text-2xl font-bold mt-4 mb-2 text-primary">${name}</h2>
       <div class="divider my-2"></div>
       ${desc ? `<p class="my-3 text-base">${desc}</p><div class="divider my-2"></div>` : ''}
       <p class="text-sm text-base-content my-2">${address}</p>

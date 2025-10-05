eae1170d43c62cbb4dab225a64ed8856af26c38d
Stanislav Svoboda <52444330+nuclearkofola@users.noreply.github.com>
2025-10-05 19:15:20 +0200

    reset

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { reactRouter } from "@react-router/dev/vite";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
-
-export default defineConfig({
-  plugins: [tailwindcss(), react(), reactRouter(), netlifyPlugin()],
-});
